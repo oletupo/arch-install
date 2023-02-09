33bce24d6b59d8f81cc2264c523075227b3e50bc
oletupo <gacami@protonmail.com>
2023-02-10 00:51:29 +0100

    cleanup

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# arch-install
+# vm-archinstall
+
+## ARCHINSTALL
+uefi
+ext4
+systemdboot
+minimal
+multilib
+extra pkg: git, vim
+---
+
+## qtile.sh
+* pacman
+xorg
+xf86-video-intel # xf86-video-fbdev
+qtile
+picom
+lightdm
+lightdm-gtk-greeter
+lxappearance
+nitrogen
+thunar
+firefox
+xfce4-terminal
+alacritty
+rofi
+arc-gtk-theme
+networkmanager
+network-manager-applet
+
+* services
+lightdm
+NetworkManager
+
+* config files
+qtile
+alacritty 
+rofi
+geany
+thunar
+
+
+* todo
+reflector service
--- vm-archinstall/qtile.sh
+++ vm-archinstall/qtile.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 sudo pacman -Syyu --noconfirm
-sudo pacman -S --noconfirm reflector xorg xf86-video-fbdev lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings firefox arc-gtk-theme arc-icon-theme lxappearance nitrogen qtile picom thunar rofi alacritty xfce4-terminal networkmanager network-manager-applet numlockx
+sudo pacman -S --noconfirm reflector xorg xf86-video-fbdev lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings firefox arc-gtk-theme arc-icon-theme lxappearance nitrogen qtile picom thunar rofi alacritty xfce4-terminal networkmanager network-manager-applet numlockx geany
 sudo reflector -c Spain -a 12 --sort rate --save /etc/pacman.d/mirrorlist
 
 sudo systemctl enable lightdm
 sudo systemctl enable NetworkManager
 
-cp -Rf .config ~/.config
+cp -Rf .config ~/
+sudo chmod+x ~/.config/qtile/scripts/autostart.sh
 
 /bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
 sleep 5
