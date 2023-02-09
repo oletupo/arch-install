66915da83dcf6e131ec518d4d6919dc2f82fa601
oletupo <gacami@protonmail.com>
2023-02-09 16:28:09 +0100

    test

--- vm-archinstall/qtile.sh
+++ vm-archinstall/qtile.sh
@@ -23,7 +23,7 @@
 sudo pacman -S --noconfirm reflector xorg xf86-video-fbdev lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings firefox arc-gtk-theme arc-icon-theme lxappearance nitrogen qtile picom thunar rofi alacritty xfce4-terminal networkmanager network-manager-applet numlockx
 
 sudo systemctl enable lightdm
-sudo systemctl
+
 /bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
 sleep 5
 sudo reboot
