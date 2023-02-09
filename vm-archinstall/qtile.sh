0e1d6f1a0ad35904046eabb80d6f25d696e8a093
oletupo <gacami@protonmail.com>
2023-02-10 00:17:35 +0100

    copy conf files, execution qtile autostart script

--- vm-archinstall/qtile.sh
+++ vm-archinstall/qtile.sh
@@ -6,6 +6,8 @@ sudo reflector -c Spain -a 12 --sort rate --save /etc/pacman.d/mirrorlist
 sudo systemctl enable lightdm
 sudo systemctl enable NetworkManager
 
+cp -Rf .config ~/.config
+
 /bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
 sleep 5
 sudo reboot
