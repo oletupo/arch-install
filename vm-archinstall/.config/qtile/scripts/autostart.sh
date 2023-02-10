fc4a6e4cb4d22fc46b8bf5f75f0a10edadf73e31
oletupo <gacami@protonmail.com>
2023-02-10 12:00:09 +0100

    xrandr resolution

--- vm-archinstall/.config/qtile/scripts/autostart.sh
+++ vm-archinstall/.config/qtile/scripts/autostart.sh
@@ -7,26 +7,17 @@ function run {
   fi
 }
 
-#Set your native resolution IF it does not exist in xrandr
-#More info in the script
-#run $HOME/.config/qtile/scripts/set-screen-resolution-in-virtualbox.sh
+# Resolution
+xrandr --output Virtual-1 --mode 1920x1080
 
-#Find out your monitor name with xrandr or arandr (save and you get this line)
-#xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal
-#xrandr --output DP2 --primary --mode 1920x1080 --rate 60.00 --output LVDS1 --off &
-#xrandr --output LVDS1 --mode 1366x768 --output DP3 --mode 1920x1080 --right-of LVDS1
-#xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
-# vertical monitor setup
-#xrandr --output DP-1 --rotate right
-
-#change your keyboard if you need it
+# Keyboard
 setxkbmap -layout es
 
 #IN BETA PHASE
 #start sxhkd to replace Qtile native key-bindings
 #run sxhkd -c ~/.config/qtile/sxhkd/sxhkdrc &
 
-#starting utility applications at boot time
+# Starting utility applications at boot time
 run nm-applet &
 #run pamac-tray &
 #run xfce4-power-manager &
@@ -36,9 +27,6 @@ picom --config $HOME/.config/qtile/scripts/picom.conf &
 #/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 #/usr/lib/xfce4/notifyd/xfce4-notifyd &
 
-#starting user applications at boot time
-#run volumeicon &
+# Starting user applications at boot time
 nitrogen --restore &
-#run firefox &
-#run thunar &
-#run dropbox &
+
